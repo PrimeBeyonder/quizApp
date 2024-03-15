5b5dc20b1ab64fd65a472b98f1f407a9a205f7d2
Andrew <thum14252@gmail.com>
2024-03-15 14:41:52 +0630

    stractur of quiz app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
+import React, { useState } from 'react';
+import QuestionCard from "./components/QuestionCard";
+const TOTAL_QUESTION = 10;
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [question, setQuestion] = useState([]);
+  const [number, setNumber] = useState(0);
+  const { userAnswer, setUserAnswer } = useState([]);
+  const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(true);
+  const startTrivia = async () => {
 
+  }
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
 
-function App() {
+  }
 
+  const nextQuestion = () => {
 
+  }
   return (
-    <div>
-
+    <div className='App'>
+      <h1>React Quiz </h1>
+      <button className='start' onCanPlay={startTrivia}>
+        Start
+      </button>
+      <p className='score'>Score : </p>
+      <p>Loading Question ....</p>
+      <QuestionCard
+        questionNumb={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
+      <button className='next' onCanPlay={nextQuestion}>Next</button>
     </div>
   )
 }
